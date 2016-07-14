890f4a3881a486ab7d2223a8b5ddda1c8f165bea
Sergei Bugrov <sergei.v.bugrov@gmail.com>
2016-07-13 20:38:59 -0600

    function rna_to_aa(ran) added

--- biutils.js
+++ biutils.js
@@ -3,6 +3,32 @@ var myHeading = document.getElementById("demo");
 var text = 'TAATGCCATGGGATGTT';
 var number = 3;
 
+function rna_to_aa(rna){
+  /*Translate an RNA string into an amino acid string.
+    Input: An RNA string Pattern.
+    Output: The translation of Pattern into an amino acid string Peptide.*/
+  
+  var rna_codon_table = {'UUU':'F', 'UUC':'F', 'UUA':'L', 'UUG':'L', 'UCU':'S',
+                         'UCC':'S', 'UCA':'S', 'UCG':'S', 'UAU':'Y', 'UAC':'Y', 
+                         'UAA':'*', 'UAG':'*', 'UGU':'C', 'UGC':'C', 'UGA':'*', 
+                         'UGG':'W', 'CUU':'L', 'CUC':'L', 'CUA':'L', 'CUG':'L', 
+                         'CCU':'P', 'CCC':'P', 'CCA':'P', 'CCG':'P', 'CAU':'H', 
+                         'CAC':'H', 'CAA':'Q', 'CAG':'Q', 'CGU':'R', 'CGC':'R', 
+                         'CGA':'R', 'CGG':'R', 'AUU':'I', 'AUC':'I', 'AUA':'I', 
+                         'AUG':'M', 'ACU':'T', 'ACC':'T', 'ACA':'T', 'ACG':'T', 
+                         'AAU':'N', 'AAC':'N', 'AAA':'K', 'AAG':'K', 'AGU':'S', 
+                         'AGC':'S', 'AGA':'R', 'AGG':'R', 'GUU':'V', 'GUC':'V', 
+                         'GUA':'V', 'GUG':'V', 'GCU':'A', 'GCC':'A', 'GCA':'A', 
+                         'GCG':'A', 'GAU':'D', 'GAC':'D', 'GAA':'E', 'GAG':'E', 
+                         'GGU':'G', 'GGC':'G', 'GGA':'G', 'GGG':'G'};
+  var aa = [];
+
+  for (i = 0; i < rna.length; i+= 3) {
+    aa.push(rna_codon_table[rna.substring(i, i+3)]);
+  }
+  
+  return aa.join('');
+}
 
 function kmers(k, dna) {
   
